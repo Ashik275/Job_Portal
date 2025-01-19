818dee50f4e37db216e225f4f7ba59654615dc7c
Ashik275 <karzon275@gmail.com>
2025-01-20 00:58:58 +0600

    upto login done

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -14,36 +15,64 @@ class AccountController extends Controller
         return view('front.account.registration');
     }
     ## This method will save a user 
-    public function processRegistration(Request $request){
-     $validator = Validator::make($request->all(),[
-        'name' => 'required',
-        'email' => 'required|email|unique:users,email',
-        'password' => 'required|min:5|same:confirm_password',
-        'confirm_password' => 'required',
-     ]);
-     if($validator->passes()){
-        $user = new User();
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = Hash::make($request->password);
-        $user->save();
-
-        session()->flash('success','You have registered successfully');
-
-        return response()->json([
-            'status' => true,
-            'errors' => []
+    public function processRegistration(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:5|same:confirm_password',
+            'confirm_password' => 'required',
         ]);
+        if ($validator->passes()) {
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->save();
 
-     }else{
-        return response()->json([
-            'status' => false,
-            'errors' => $validator->errors()
-        ]);
-     }
+            session()->flash('success', 'You have registered successfully');
+
+            return response()->json([
+                'status' => true,
+                'errors' => []
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
     }
-    public function login() {
+    public function login()
+    {
         return view('front.account.login');
+    }
 
+    public function authenticate(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        if ($validator->passes()) {
+            if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+                return redirect()->route('account.profile');
+            } else {
+                return redirect()->route('account.login')->with('error', 'Email/Password is incorrect')->withInput($request->only('email'));
+            }
+        } else {
+            return redirect()->route('account.login')->withErrors($validator)->withInput($request->only('email'));
+        }
+    }
+
+    public function profile()
+    {
+        return view('front.account.profile');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('account.login');
     }
 }
