dafc810429e84be4f3ff52f7dcc2d24497041d77
Ashik275 <karzon275@gmail.com>
2025-02-23 23:39:55 +0600

    till job page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\JobsController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -10,6 +11,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/jobs', [JobsController::class, 'index'])->name('jobs');
 
 
 
@@ -37,6 +39,5 @@ Route::group(['account'], function () {
     Route::post('/account/delete-job', [AccountController::class, 'deleteJob'])->name('account.deleteJob');
 
     Route::get('/account/my-jobs', [AccountController::class, 'myJobs'])->name('account.myJobs');
-
   });
 });
